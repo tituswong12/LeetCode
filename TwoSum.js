948c136c7c636a66dadcc7107ccdd98dc2d3660c
Titus Wong <117533818+tituswong12@users.noreply.github.com>
2023-01-22 22:12:40 -0500

    Add files via upload

---
+++ TwoSum.js
@@ -0,0 +1,13 @@
+var twoSum = function(nums, target) {
+    let output =[]
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j<nums.length;j++){
+            if(nums[i]+nums[j]===target){
+                output = [i,j]
+            }
+        }
+    
+    }
+    return output
+    
+}
